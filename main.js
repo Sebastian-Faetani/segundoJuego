4d2f2c64c3c0cf01345e08cff65dcb850e3aea05
Sebastian Faetani <128256756+Sebastian-Faetani@users.noreply.github.com>
2023-05-30 18:35:41 -0300

    Modificar tilemap grande

---
+++ main.js
@@ -0,0 +1,38 @@
+import Nivel1 from "./src/scenes/Nivel1.js";
+import Nivel2 from "./src/scenes/Nivel2.js";
+
+import Precarga from "./src/scenes/Precarga.js";
+import Fin from "./src/scenes/Fin.js";
+
+// Create a new Phaser config object
+const config = {
+  type: Phaser.AUTO,
+  width: 800,
+  height: 600,
+  scale: {
+    mode: Phaser.Scale.FIT,
+    autoCenter: Phaser.Scale.CENTER_BOTH,
+    min: {
+      width: 800,
+      height: 600,
+    },
+    max: {
+      width: 1600,
+      height: 1200,
+    },
+  },
+  physics: {
+    default: "arcade",
+    arcade: {
+      gravity: { y: 800 },
+      debug: true,
+    },
+  },
+  // List of scenes to load
+  // Only the first scene will be shown
+  // Remember to import the scene before adding it to the list
+  scene: [Precarga, Nivel1, Nivel2, Fin],
+};
+
+// Create a new Phaser game instance
+window.game = new Phaser.Game(config);
